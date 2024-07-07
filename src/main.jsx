83f69aae51b2670857c7c789c4295137243590e7
Santiago Montironi <119986777+santimontironi@users.noreply.github.com>
2024-07-07 17:18:07 -0300

    Se han agregado comentarios.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import './hover.css'
+import './index.css'
 import { Home } from './containers/Home';
 import { Services } from './containers/Services'
 import { About } from './containers/AboutUs'
